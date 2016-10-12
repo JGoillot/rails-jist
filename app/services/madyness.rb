2a2f6a0cc47f2378ff312448e798375d22c381c1
JFachetti <goillot.jeremy@gmail.com>
2016-10-12 09:57:21 +0200

    madyness done

--- app/services/madyness.rb
+++ app/services/madyness.rb
@@ -19,7 +19,7 @@ class Madyness < ServiceBase
 
     xml_doc.search('item').each do |article|
 
-
+      # Journalist Parsing
       full_name = article.xpath('dc:creator').text
       full_name.split(" ")
 
@@ -28,8 +28,25 @@ class Madyness < ServiceBase
         last_name: full_name.split(" ")[1],
         company: "Madyness"
       }
+      journalist = Journalist.where(journalist_data).first_or_create
 
-      Journalist.create(journalist_data)
+      # Keyword Parsing
+      article.xpath('category').each do |k|
+        keyword = Keyword.where(name: k.text).first_or_create
+        journalist.keywords << keyword
+      end
+
+      # Article Parsing
+      article_data = {
+        title: article.xpath('title').text,
+        link: article.xpath('link').text,
+        writed_at: article.xpath('pubDate').text,
+        journalist: journalist
+      }
+
+      article = Article.find_or_create_by(article_data)
+
+
     end
 
   end
