5facd667c59a669d04854b29b304d1be48eabba9
JFachetti <goillot.jeremy@gmail.com>
2016-10-25 23:28:06 +0200

    Each with journalist & Data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011144513) do
+ActiveRecord::Schema.define(version: 20161025165808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20161011144513) do
     t.string   "company"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "photo_url"
+    t.text     "biography"
   end
 
   create_table "keywords", force: :cascade do |t|
